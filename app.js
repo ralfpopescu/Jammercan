979135f34abacfd6e6674763e9d2000bcfd7b9e6
Ralf Popescu <ralfpopescu@gmail.com>
2015-09-20 23:07:26 -0400

    fix error

--- app.js
+++ app.js
@@ -36,6 +36,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(process.env.PORT || 3000;, function(){
+http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
