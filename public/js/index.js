e0a0e89d76008a8574de7090b2a8164a113a9526
Ralf Popescu <ralfpopescu@gmail.com>
2015-09-20 23:20:04 -0400

    add jquery

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,6 @@
 var socket = io();
 socket.emit('message', 'hello whats up');
 socket.on('message', function(msg) {
-  var messages = document.getElementById('messages');
-  var newMessage = document.createTextNode(msg);
-  messages.appendChild(newMessage);
+  var messages = $('#messages');
+  messages.append('<li>' + msg + '</li>')
 });
