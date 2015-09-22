78fd68da32b61a160b656851de247b8c36803f5c
Maximillian von Briesen <mobyvb@gmail.com>
2015-09-21 22:12:10 -0400

    add sample based instruments

--- public/js/sound.js
+++ public/js/sound.js
@@ -11,7 +11,40 @@ oscillator.type = 'sine';
 oscillator.frequency.value = frequency;
 gainNode.gain.value = volume;
 
-oscillator.start();
-setTimeout(function() {
-  oscillator.stop();
-}, 1000);
+// oscillator.start();
+// setTimeout(function() {
+//   oscillator.stop();
+// }, 1000);
+
+var grandPiano = MIDI.Soundfont.acoustic_grand_piano;
+var grandPianoBuffers = {};
+for (var key in grandPiano) {
+  addNoteToBuffer(key, grandPiano, grandPianoBuffers);
+}
+
+function addNoteToBuffer(key, instrumentData, instrumentBuffers) {
+  var noteBuffer = base64ToArrayBuffer(instrumentData[key]);
+  audioCtx.decodeAudioData(noteBuffer, function(buffer) {
+    grandPianoBuffers[key] = buffer;
+    if (Object.keys(instrumentData).length === Object.keys(instrumentBuffers).length) {
+      console.log('done');
+    }
+  });
+}
+
+function getAudioBuffer(key, instrumentBuffers) {
+  var sound = audioCtx.createBufferSource();
+  sound.buffer = instrumentBuffers[key];
+  sound.connect(audioCtx.destination);
+  return sound;
+}
+
+function base64ToArrayBuffer(base64) {
+  var binary_string =  window.atob(base64);
+  var len = binary_string.length;
+  var bytes = new Uint8Array( len );
+  for (var i = 0; i < len; i++)        {
+    bytes[i] = binary_string.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
